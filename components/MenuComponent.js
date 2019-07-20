bdd585bb119ca45ee28b75b2a754cf50a9db1f22
Jon Chanyoung Yoo <jonyoowa@outlook.com>
2019-07-19 18:19:15 -0700

    Activity Indicator

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -4,6 +4,7 @@ import { Tile } from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -26,18 +27,39 @@ class Menu extends Component {
     };
 
     render() {
-
         const renderMenuItem = ({item, index}) => {
-            return (
-                <Tile
-                    key={index}
-                    title={item.name}
-                    caption={item.description}
-                    featured
-                    onPress={() => navigate('DishDetail', { dishId: item.id })}
-                    imageSrc={{ uri: baseUrl + item.image}}
-                    />
-            );
+
+            if (this.props.dishes.isLoading) {
+                return(
+                    <Loading />
+                );
+            }
+            else if (this.props.dishes.errMess) {
+                return(
+                    <View>            
+                        <Text>{props.dishes.errMess}</Text>
+                    </View>            
+                );
+            }
+            else {
+                return (
+                    <FlatList 
+                        data={this.props.dishes.dishes}
+                        renderItem={renderMenuItem}
+                        keyExtractor={item => item.id.toString()}
+                        />
+                );
+            }
+            // return (
+            //     <Tile
+            //         key={index}
+            //         title={item.name}
+            //         caption={item.description}
+            //         featured
+            //         onPress={() => navigate('DishDetail', { dishId: item.id })}
+            //         imageSrc={{ uri: baseUrl + item.image}}
+            //         />
+            // );
         };
 
         const { navigate } = this.props.navigation;
