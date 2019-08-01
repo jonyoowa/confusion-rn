932068db72bddc4d19b238eb9ede2c5e4f286fba
Jon Yoo <jonyoowa@outlook.com>
2019-08-01 13:15:40 -0700

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -5,6 +5,7 @@ import DatePicker from 'react-native-datepicker'
 
 import * as Animatable from 'react-native-animatable';
 import Swipeout from 'react-native-swipeout';
+import { Permissions, Notifications } from 'expo';
 
 class Reservation extends Component {
 
@@ -35,7 +36,35 @@ class Reservation extends Component {
             date: ''
         });
     }
-    
+
+    async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for '+ date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
+        });
+    }
+
     giveAlert() {
         Alert.alert(
             'Your Reservation OK?',
